ccf4a383da475828f3623a6d4cc24661c43b9f95
stasi815 <anastasia.gallardo@students.makeschool.com>
2019-12-05 23:57:32 -0800

    collectstatic

--- makewiki/settings.py
+++ makewiki/settings.py
@@ -127,8 +127,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # wiki app settings
 WIKI_PAGE_TITLE_MAX_LENGTH = 600
@@ -150,4 +156,4 @@ try:
 except ImportError:
     pass
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
